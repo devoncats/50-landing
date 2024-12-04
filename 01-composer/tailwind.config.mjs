5cd93c311c0ebeb8eef3d76e9ea82405320b68ce
devoncats <jivasquezr2021@gmail.com>
2024-12-04 18:25:05 -0500

    Add Hero and Logos components, update layout and Tailwind configuration

--- 01-composer/tailwind.config.mjs
+++ 01-composer/tailwind.config.mjs
@@ -1,8 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        white: '#ffffff',
+        black: {
+          300: '#bdbdbd',
+          700: '#282828',
+          900: '#000000',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
